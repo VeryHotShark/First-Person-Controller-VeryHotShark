e1cbcd597fb39ab3759357c7e7ba6e5d0df7c75f
ELUKTRONIKS\Brenton <brentonpharr@gmail.com>
2019-06-11 21:01:10 -0700

    Marked all methods as virtual, marked private fields and methods as Protected to allow for extension via Inheritance

--- FirstPersonController/Assets/Scripts/Scriptable_Objects/MovementInputData.cs
+++ FirstPersonController/Assets/Scripts/Scriptable_Objects/MovementInputData.cs
@@ -6,16 +6,16 @@ namespace VHS
     public class MovementInputData : ScriptableObject
     {
         #region Data
-            Vector2 m_inputVector;
+            protected Vector2 m_inputVector;
 
-            bool m_isRunning;
-            bool m_isCrouching;
+            protected bool m_isRunning;
+            protected bool m_isCrouching;
 
-            bool m_crouchClicked;
-            bool m_jumpClicked;
+            protected bool m_crouchClicked;
+            protected bool m_jumpClicked;
 
-            bool m_runClicked;
-            bool m_runReleased;
+            protected bool m_runClicked;
+            protected bool m_runReleased;
         #endregion
 
         #region Properties
@@ -69,7 +69,7 @@ namespace VHS
         #endregion
 
         #region Custom Methods
-            public void ResetInput()
+            public virtual void ResetInput()
             {
                 m_inputVector = Vector2.zero;
                 
@@ -79,7 +79,7 @@ namespace VHS
                 m_crouchClicked = false;
                 m_jumpClicked = false;
                 m_runClicked = false;
-                m_runReleased =false;
+                m_runReleased = false;
             }
         #endregion
     }
