337e917d4301eeded1e5104f1f13f9b8322690aa
sena_kuzuoka <senakuzuoka@gmail.com>
2021-05-08 14:14:15 +0900

    Add Button Sample

---
+++ FirstPersonController/Assets/Samples/Scripts/ButtonSample.cs
@@ -0,0 +1,47 @@
+﻿using Cysharp.Threading.Tasks;
+using System;
+using System.Threading;
+using UnityEngine;
+
+namespace Sample
+{
+    public class ButtonSample : MonoBehaviour
+    {
+        [SerializeField] MeshRenderer _mesh;
+        [SerializeField] Collider _collider;
+
+        CancellationTokenSource _cts = default;
+
+        public void OnButtonSelected()
+        {
+            _cts?.Cancel();
+
+            UniTask.Void(async () =>
+            {
+                _cts = new CancellationTokenSource();
+                var ct = _cts.Token;
+
+                try
+                {
+                    ct.ThrowIfCancellationRequested();
+                    _mesh.enabled = false;
+                    _collider.enabled = false;
+                    await UniTask.Delay(TimeSpan.FromSeconds(1), cancellationToken: ct);
+                    ct.ThrowIfCancellationRequested();
+                    _mesh.enabled = true;
+                    _collider.enabled = true;
+                }
+                finally
+                {
+                    _cts = default;
+                }
+            });
+        }
+
+        private void Reset()
+        {
+            _mesh = transform.GetComponent<MeshRenderer>();
+            _collider = transform.GetComponent<Collider>();
+        }
+    }
+}
