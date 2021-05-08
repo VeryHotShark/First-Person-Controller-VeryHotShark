5fa6f0d879b8ad5848a1e2d11e4c8ac505e2c0df
sena_kuzuoka <senakuzuoka@gmail.com>
2021-05-08 15:42:58 +0900

    add ToggleButton sample

---
+++ FirstPersonController/Assets/Samples/Scripts/ToggleButtonSample.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using VHS;
+
+namespace Sample
+{
+    public class ToggleButtonSample : MonoBehaviour
+    {
+        [SerializeField] MeshRenderer _mesh;
+        [SerializeField] Collider _collider;
+
+        public void OnToggleButtonSelected(ToggleButtonInteractable toggleButtonInteractable)
+        {
+            _mesh.enabled = toggleButtonInteractable.IsOn;
+            _collider.enabled = toggleButtonInteractable.IsOn;
+        }
+
+        private void Reset()
+        {
+            _mesh = transform.GetComponent<MeshRenderer>();
+            _collider = transform.GetComponent<Collider>();
+        }
+    }
+}
