4a7bdea260ac6b4dd7ae75fab0988624decd05da
sena_kuzuoka <senakuzuoka@gmail.com>
2021-05-09 15:02:52 +0900

    簡易的なAudioServiceを実装

---
+++ FirstPersonController/Assets/Scripts/Service/Audio/AudioMaster.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace VHS.Audio
+{
+    [CreateAssetMenu(fileName = "AudioMaster", menuName = "FirstPersonController/Audio/AudioMaster", order = 0)]
+    public class AudioMaster : ScriptableObject
+    {
+        public static string FilePath
+        {
+            get { return "Audio/Master/AudioMaster.asset"; }
+        }
+
+        public List<AudioClip> _seAudioClips = new List<AudioClip>();
+    }
+}
---
+++ FirstPersonController/Assets/Scripts/Service/Audio/AudioService.cs
@@ -0,0 +1,32 @@
+﻿using Cysharp.Threading.Tasks;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+namespace VHS.Audio
+{
+    public class AudioService
+    {
+        Dictionary<string, AudioClip> _audioDatas = new Dictionary<string, AudioClip>();
+
+        public async UniTask Initialize()
+        {
+            await Preload();
+        }
+
+        async UniTask Preload()
+        {
+            var master = await Addressables.LoadAssetAsync<AudioMaster>(AudioMaster.FilePath);
+            foreach (var audioClip in master._seAudioClips)
+            {
+                _audioDatas.Add(audioClip.name, audioClip);
+            }
+        }
+
+        public void PlaySe(string key, AudioSource audioSource)
+        {
+            if (!_audioDatas.ContainsKey(key)) return;
+            audioSource.PlayOneShot(_audioDatas[key]);
+        }
+    }
+}
